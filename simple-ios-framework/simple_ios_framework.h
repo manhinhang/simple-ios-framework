ec15fbaef2ca0f89815ec14e5df9b0ac8a9d1985
Andrew Man <andrewman@indooratlas.com>
2020-06-11 17:17:34 +0800

    create ios porject

---
+++ simple-ios-framework/simple_ios_framework.h
@@ -0,0 +1,19 @@
+//
+//  simple_ios_framework.h
+//  simple-ios-framework
+//
+//  Created by man Hin Hang on 11/6/2020.
+//  Copyright © 2020 man Hin Hang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for simple_ios_framework.
+FOUNDATION_EXPORT double simple_ios_frameworkVersionNumber;
+
+//! Project version string for simple_ios_framework.
+FOUNDATION_EXPORT const unsigned char simple_ios_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <simple_ios_framework/PublicHeader.h>
+
+
